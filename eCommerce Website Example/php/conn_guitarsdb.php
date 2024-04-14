afbfd33741b8ccac8f734e966f39b9045426db82
SimonWJohnson <simonwjohnson@gmail.com>
2024-04-14 18:07:54 +0930

    Amended connection string

--- eCommerce Website Example/php/conn_guitarsdb.php
+++ eCommerce Website Example/php/conn_guitarsdb.php
@@ -1,10 +1,14 @@
 <?php
 // Data access class/script for guitarsdb
 //$server = "localhost:3307"; // Double check this for home XAMMP server
-$server = "sql307.infinityfree.com:3306";
-$user = "root";
-$password = "";
+//$user = "root";
+//$password = "";
 //$database = "guitars.com";
+
+// InfinityFree Credentials
+$server = "sql307.infinityfree.com";
+$user = "if0_35802201";
+$password = "LmHzjMU9qkGs";
 $database = "if0_35802201_guitars_com";
 
 // Connect to the MySQL server/db
